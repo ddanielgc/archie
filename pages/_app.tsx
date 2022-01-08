b46b8c0936864c92521921cde6ed48ae8eeb0e77
daniel.campos <daniel.campos@caminoeducation.com>
2022-01-08 17:39:01 -0300

    chore(): Using custom Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { ApolloProvider } from '@apollo/client'
 import client from '@common/apollo-client'
+import theme from '@common/styles/theme'
 
 export default function MyApp({ Component, pageProps }) {
 
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider>
-        <Component {...pageProps} />
+    <ApolloProvider client={ client } >
+      <ChakraProvider theme={ theme } >
+        <Component { ...pageProps } />
       </ChakraProvider>
     </ApolloProvider>
   )
