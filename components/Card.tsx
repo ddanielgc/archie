9cf6f0e0fccfb8656192aed433d533623395e8c5
daniel.campos <daniel.campos@caminoeducation.com>
2022-01-08 17:40:00 -0300

    chore(): formatting

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,5 @@
 import { ILaunch } from '@common/interfaces/launch.interface'
-import { Box, Link } from '@chakra-ui/react'
+import { Box, Link, Center, Tooltip, Text } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import Image from 'next/image'
 
@@ -9,28 +9,29 @@ interface ILaunchCardProps {
 
 const Card: React.FC<ILaunchCardProps> = ({ launch }: ILaunchCardProps) => {
   return (
-      <Box 
-        key={launch?.id} 
+      <Box
+        key={ launch?.id }
         maxW='sm'
-        borderRadius='lg' 
-        overflow='hidden' 
-        boxShadow='base' 
-        rounded='md' 
+        borderRadius='lg'
+        overflow='hidden'
+        boxShadow='base'
+        rounded='md'
         bg='white'
       >
-        <Box display='flex' alignItems='baseline'></Box>
+        {/* <Box display='flex' alignItems='baseline'></Box> */}
         <Box 
-          overflow='hidden' 
+          overflow='hidden'
           boxShadow='base' 
           bg='gray.100'
-          paddingTop={'10px'}
         >
-          <Image
-            src={launch?.links?.mission_patch} 
-            width={200}
-            height={200}
-            alt={launch?.mission_name}
-          />
+          <Center p={2}>
+            <Image
+              src={ launch?.links?.mission_patch } 
+              width={200}
+              height={200}
+              alt={ launch?.mission_name }
+            />
+          </Center>
         </Box>
         <Box p='2' alignItems='baseline'>
           <Box display='flex' alignItems='flex-start' justifyContent='space-between'>
@@ -41,24 +42,26 @@ const Card: React.FC<ILaunchCardProps> = ({ launch }: ILaunchCardProps) => {
               lineHeight='tight'
               isTruncated={true}
             >
-              {launch?.mission_name}
+              Mission: { launch?.mission_name }
             </Box>
-            <Link href={launch?.links?.article_link}  isExternal>
+            <Link href={ launch?.links?.article_link }  isExternal>
               <ExternalLinkIcon mx='2px' />
             </Link>
           </Box>
-          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-            {launch?.rocket?.rocket_name}
+          <Box as='span' marginTop={1}>
+            <Text fontSize='sm' color='gray.600' fontWeight='semibold'>Rocket: { launch?.rocket?.rocket_name }</Text>
           </Box>
-          <Box
-            mt='1'
-            fontWeight='bold'
-            as='h4'
-            lineHeight='tight'
-            isTruncated={true}
-          >
-            {launch?.details || 'No details available'}
-          </Box>
+          <Tooltip hasArrow label={ launch?.details } bg='gray.300' color='black' placement='auto'>
+            <Box
+              mt='1'
+              fontWeight='bold'
+              as='h4'
+              lineHeight='tight'
+              isTruncated={true}
+            >
+              { launch?.details || 'No details available' }
+            </Box>
+          </Tooltip>
         </Box>
       </Box>
   )
